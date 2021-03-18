629292fc0f21c41fb25bd2b6204fb1b94b545d41
Developer1510 <developer1510@sinergise.com>
2021-03-18 13:13:48 +0100

    delete dist

--- dist/js/tags.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// Listen for clicks on the tags and send to appropriate page if clicked.
-$('.tag').click(function () {
-  let location = '/tag/' + $(this).text().replace(/ /g, '-');
-  window.location.href = location;
-});
