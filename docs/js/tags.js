d4a3f0dcaa09c65bc91e2e8acd43a2f2b225af36
AWS CodeBuild <codebuild@aws.amazon.com>
2021-03-18 15:52:07 +0000

    [CodeBuild]

--- docs/js/tags.js
+++ docs/js/tags.js
@@ -2,6 +2,6 @@
 
 // Listen for clicks on the tags and send to appropriate page if clicked.
 $('.tag').click(function () {
-  let location = 'tag/' + $(this).text().replace(/ /g, '-');
+  let location = rootUrl + 'tag/' + $(this).text().replace(/ /g, '-');
   window.location.href = location;
 });
