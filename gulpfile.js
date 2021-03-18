3172d7d92dff017877cf3e002d6baf64e89dc91f
Developer1510 <developer1510@sinergise.com>
2021-03-18 18:27:06 +0100

    test

--- gulpfile.js
+++ gulpfile.js
@@ -738,7 +738,7 @@ function htmlDetail () {
       var templateData = JSON.parse(file.contents.toString('utf8'));
       templateData.rootUrl = process.env.ROOT_URL;
       templateData.githubRepo = process.env.GIT_HUB_REPO;
-      templateDate.githubBranch = process.env.GIT_HUB_BRANCH;
+      templateData.githubBranch = process.env.GIT_HUB_BRANCH;
       // If we have no DataAtWork, remove it
       if (!templateData.DataAtWork || (templateData.DataAtWork && _.compact(_.flatMap(templateData.DataAtWork)).length === 0)) {
         delete templateData.DataAtWork;
